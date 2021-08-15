7466f49802d2deaf15a68cf155746bd28792d100
Dyupich <warrior2001@mail.ru>
2021-08-15 11:24:14 +0300

    init

---
+++ pythonProject/main.py
@@ -0,0 +1,17 @@
+print("Введите число а")
+a = float(input())
+print("Введите число b")
+b = float(input())
+print("Введите операцию")
+c = input()
+
+if c == '+':
+    print("Результат: ", a + b)
+if c == '-':
+    print("Результат: ", a - b)
+if c == '/':
+    print("Результат: ", a / b)
+if c == '*':
+    print("Результат: ", a * b)
+if c == '%':
+    print("Результат: ", a % b)
